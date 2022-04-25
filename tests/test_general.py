eb173f910b9a23d8a6fe5875d07306e12a54be60
Elena <elena_stepuro@epam.com>
2022-04-25 18:43:17 +0300

    CI-test

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,3 +1,5 @@
+from selenium import webdriver
+
 from src.main.pages.testenv_page import TestEnvPage
 from tests.test_base import TestBase
 
@@ -5,7 +7,13 @@ from tests.test_base import TestBase
 class TestGeneral(TestBase):
 
     def test_select_checkboxes(self, setup_method):
-        self.driver = setup_method
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Remote(
+            command_executor="http://localhost:8085",
+            desired_capabilities=webdriver.DesiredCapabilities.CHROME,
+            options=options)
         test_page = TestEnvPage(self.driver)
 
         test_page.open_browser()
