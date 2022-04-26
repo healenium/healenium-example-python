884603ff844b90d200f0ab211674cfc725a37d01
Elena <elena_stepuro@epam.com>
2022-04-26 17:46:57 +0300

    Comment wait test

--- tests/test_wait.py
+++ tests/test_wait.py
@@ -14,8 +14,8 @@ class TestWait:
         markup_page.click_test_button() #should be healed
         markup_page.confirm_alert()
 
-        markup_page.generate_markup()
-        markup_page.click_test_button_wait(5) #should be healed
-        markup_page.confirm_alert()
+        # markup_page.generate_markup()
+        # markup_page.click_test_button_wait(5) #should be healed
+        # markup_page.confirm_alert()
 
         markup_page.close()
