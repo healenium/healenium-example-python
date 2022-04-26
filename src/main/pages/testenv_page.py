606a509fb1dc3588088f1a7ae0ab8005dfdef243
Elena <elena_stepuro@epam.com>
2022-04-26 15:42:10 +0300

    Refactor test env page

--- src/main/pages/testenv_page.py
+++ src/main/pages/testenv_page.py
@@ -1,3 +1,6 @@
+import logging
+
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 from src.main.pages.base_page import BasePage
@@ -7,6 +10,18 @@ from src.main.search.context import Context
 class TestEnvPage(BasePage):
     submit_btn = 'Submit'
     submit_form_btn = 'Submit_checkbox'
+
+    driver: webdriver
+
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Remote(
+            command_executor="http://localhost:8085",
+            desired_capabilities=webdriver.DesiredCapabilities.CHROME,
+            options=options)
+        # self.driver=webdriver.Chrome()
 
     def open_browser(self):
         self.driver.get(BasePage.testEnvPageUrl)
@@ -34,5 +49,9 @@ class TestEnvPage(BasePage):
         return self
 
     def find_test_element(self, locator_type, selector):
+        logging.info("Find element By ")
         result = Context().set_strategy(self.driver, locator_type).execute_strategy(selector)
         assert result == True
+
+    def close(self):
+        self.driver.quit()
