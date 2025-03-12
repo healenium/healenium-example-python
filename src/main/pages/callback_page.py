afa4a5fdad53608cb74fa4d7cbfc6fc92bfd0f9f
Alex-Reif <69298932+Alex-Reif@users.noreply.github.com>
2025-03-12 16:49:39 +0300

    Update src/main/pages/callback_page.py

--- src/main/pages/callback_page.py
+++ src/main/pages/callback_page.py
@@ -10,7 +10,7 @@ class CallbackPage(BasePage):
     remove_square_button = '//button[contains(@class, "remove")]'
 
     test_button = '//custom-square[contains(@c, "red")]'
-    test_button_css = '[color="red"]'
+    test_button_css = 'custom-square'
 
     driver: webdriver
 
