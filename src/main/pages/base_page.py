78915cf1539a7d17c4cde6c62f8c3545ad26ca92
helen_yrofeeva <helen_yrofeeva@epam.com>
2026-03-02 19:05:26 +0300

    [EPMHLM-501] update test-env link

--- src/main/pages/base_page.py
+++ src/main/pages/base_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.alert import Alert
 class BasePage(object):
     mainPageUrl = 'https://sha-test-app.herokuapp.com/'
     callbackTestPageUrl = 'https://mdn.github.io/web-components-examples/life-cycle-callbacks/'
-    testEnvPageUrl = 'https://elenastepuro.github.io/test_env/index.html'
+    testEnvPageUrl = 'https://healenium.github.io/healenium-test-env/index.html'
 
     def __init__(self, driver):
         self.driver = driver
